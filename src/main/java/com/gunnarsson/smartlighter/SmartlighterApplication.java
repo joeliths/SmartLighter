2f22c8abad6869dd847a8293a52359bf68e4b398
Joel Gunnarsson <joel@Joels-MacBook-Pro.local>
2021-03-03 14:37:54 +0100

    Fix problem with no configured datasource, add getters/setters to UserEntity

--- src/main/java/com/gunnarsson/smartlighter/SmartlighterApplication.java
+++ src/main/java/com/gunnarsson/smartlighter/SmartlighterApplication.java
@@ -2,8 +2,9 @@ package com.gunnarsson.smartlighter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SmartlighterApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/gunnarsson/smartlighter/io/entity/UserEntity.java
+++ src/main/java/com/gunnarsson/smartlighter/io/entity/UserEntity.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.io.Serializable;
 
-@Entity(name="users")
+//@Entity(name="users")
 public class UserEntity implements Serializable {
     private static final long serialVersionUID = 5012315223526710046L;
 
@@ -25,4 +25,44 @@ public class UserEntity implements Serializable {
 
     @Column(nullable = false)
     private String encryptedPassword;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
 }
