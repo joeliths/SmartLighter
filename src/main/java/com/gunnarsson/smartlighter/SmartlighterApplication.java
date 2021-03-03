63d9f1e4ff8a0980e07e8865b2daa3d92a0d4889
Joel Gunnarsson <joel@Joels-MacBook-Pro.local>
2021-03-03 13:04:11 +0100

    initial commit

---
+++ src/main/java/com/gunnarsson/smartlighter/SmartlighterApplication.java
@@ -0,0 +1,13 @@
+package com.gunnarsson.smartlighter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlighterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartlighterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gunnarsson/smartlighter/SmartlighterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunnarsson.smartlighter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartlighterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
