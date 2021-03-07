d675e813682599a20cbb823950c0842c2cf9c5d0
Joel Gunnarsson <joel@Joels-MacBook-Pro.local>
2021-03-07 18:03:21 +0100

    Add WebSecurity class and configuration to permit sign up url

---
+++ src/main/java/com/gunnarsson/smartlighter/security/WebSecurity.java
@@ -0,0 +1,17 @@
+package com.gunnarsson.smartlighter.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/users").permitAll()
+                .anyRequest().authenticated();
+    }
+}
