e46da7f130ba6b758f6075efc26de000d661b369
Joel Gunnarsson <joel@Joels-MacBook-Pro.local>
2021-03-07 15:30:10 +0100

    Add Utils class and implement generate userId method

---
+++ src/main/java/com/gunnarsson/smartlighter/shared/Utils.java
@@ -0,0 +1,25 @@
+package com.gunnarsson.smartlighter.shared;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class Utils {
+    private final Random RANDOM = new SecureRandom();
+    private final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public String generateUserId(int length) {
+        return generateRandomString(length);
+    }
+
+    private String generateRandomString(int length) {
+        StringBuilder returnValue = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+
+        return new String(returnValue);
+    }
+}
+
+
