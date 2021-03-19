47b6c0edd6c0da71533bd9480353c0a12211917a
Joel Gunnarsson <joel@Joels-MacBook-Pro.local>
2021-03-19 13:38:27 +0100

    Add LightRequestModel class

---
+++ src/main/java/com/gunnarsson/smartlighter/ui/model/request/LightRequestModel.java
@@ -0,0 +1,22 @@
+package com.gunnarsson.smartlighter.ui.model.request;
+
+public class LightRequestModel {
+    private String lightName;
+    private String ipAddress;
+
+    public String getLightName() {
+        return lightName;
+    }
+
+    public void setLightName(String lightName) {
+        this.lightName = lightName;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+}
