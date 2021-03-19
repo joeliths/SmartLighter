ed289cc22910b1b1446c1f2d4a208656b9521c48
Joel Gunnarsson <joel@Joels-MacBook-Pro.local>
2021-03-19 13:39:40 +0100

    Add findLightByLightId method

--- src/main/java/com/gunnarsson/smartlighter/io/repositories/LightRepository.java
+++ src/main/java/com/gunnarsson/smartlighter/io/repositories/LightRepository.java
@@ -2,6 +2,9 @@ package com.gunnarsson.smartlighter.io.repositories;
 
 import com.gunnarsson.smartlighter.io.entity.LightEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LightRepository extends CrudRepository<LightEntity,Long> {
+    LightEntity findLightByLightId(String lightId);
 }
