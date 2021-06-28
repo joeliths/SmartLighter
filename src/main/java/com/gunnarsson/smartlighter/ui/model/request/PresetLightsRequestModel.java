ccd2e8ac76604e4d817ddc67b2ae1eb801d13540
Joel Gunnarsson <joel@Joels-MacBook-Pro.local>
2021-06-28 11:12:06 +0200

    Add PresetLightsRequestModel

---
+++ src/main/java/com/gunnarsson/smartlighter/ui/model/request/PresetLightsRequestModel.java
@@ -0,0 +1,26 @@
+package com.gunnarsson.smartlighter.ui.model.request;
+
+import java.util.List;
+import java.util.Set;
+
+public class PresetLightsRequestModel {
+    private String collectionName;
+    private List<LightStateModel> presets;
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
+    }
+
+    public List<LightStateModel> getPresets() {
+        return presets;
+    }
+
+    public void setPresets(List<LightStateModel> presets) {
+        this.presets = presets;
+    }
+
+}
