20ae741d77a6608774546e8746b79595f0c546bb
Joel Gunnarsson <joel@Joels-MacBook-Pro.local>
2021-06-28 11:22:23 +0200

    Add findLightbyLightId and getAllLights

--- src/main/java/com/gunnarsson/smartlighter/service/LightService.java
+++ src/main/java/com/gunnarsson/smartlighter/service/LightService.java
@@ -2,7 +2,11 @@ package com.gunnarsson.smartlighter.service;
 
 import com.gunnarsson.smartlighter.shared.dto.LightDto;
 
+import java.util.List;
+
 public interface LightService {
+    LightDto findLightByLightId(String lightId);
     LightDto createLight(LightDto light);
     String turnOn(String id);
+    List<LightDto> getAllLights();
 }
