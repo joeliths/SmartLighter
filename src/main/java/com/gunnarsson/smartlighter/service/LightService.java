c57fef6fa11e6504317cda2276671d71ad48c216
Joel Gunnarsson <joel@Joels-MacBook-Pro.local>
2021-08-24 15:00:15 +0200

    add delete method

--- src/main/java/com/gunnarsson/smartlighter/service/LightService.java
+++ src/main/java/com/gunnarsson/smartlighter/service/LightService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface LightService {
     LightDto findLightByLightId(String lightId);
     LightDto createLight(LightDto light);
+    void deleteLight(LightDto light);
     String turnOn(String id);
     String turnOff(String lightId);
     List<LightDto> getAllLights();
