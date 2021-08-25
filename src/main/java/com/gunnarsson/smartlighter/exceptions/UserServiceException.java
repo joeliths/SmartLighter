ac92fdc527610b9ff9ddf77a58f6bc3a2d28dfc8
Joel Gunnarsson <joel@Joels-MacBook-Pro.local>
2021-08-25 18:15:44 +0200

    add constructor and serialVersionUID

--- src/main/java/com/gunnarsson/smartlighter/exceptions/UserServiceException.java
+++ src/main/java/com/gunnarsson/smartlighter/exceptions/UserServiceException.java
@@ -1,2 +1,9 @@
-package com.gunnarsson.smartlighter.exceptions;public class UserServiceException {
+package com.gunnarsson.smartlighter.exceptions;
+
+public class UserServiceException extends RuntimeException{
+    private static final long serialVersionUID = 1999722213111444607L;
+
+    public UserServiceException(String message) {
+        super(message);
+    }
 }
