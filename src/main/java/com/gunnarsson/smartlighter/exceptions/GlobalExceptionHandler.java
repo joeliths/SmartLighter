927f94f4ddd2af1dc0d95001276b0ee7e9adf893
Joel Gunnarsson <joel@Joels-MacBook-Pro.local>
2021-08-25 17:34:31 +0200

    add global exception handler and implement method for light service exceptions

--- src/main/java/com/gunnarsson/smartlighter/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/gunnarsson/smartlighter/exceptions/GlobalExceptionHandler.java
@@ -1,2 +1,30 @@
-package com.gunnarsson.smartlighter.exceptions;public class GlobalExceptionHandler {
+package com.gunnarsson.smartlighter.exceptions;
+
+import com.gunnarsson.smartlighter.ui.model.response.ErrorMessage;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = {LightServiceException.class})
+    public ResponseEntity<Object> handleLightException(LightServiceException ex, WebRequest request){
+        ErrorMessage errorMessage = new ErrorMessage(new Date(),ex.getMessage());
+
+        return new ResponseEntity<>(errorMessage,new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handleOtherExceptions(Exception ex, WebRequest request)
+    {
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
